43ecc5800f30cd5516dc2c8f4f752b35ee471bed
Leon Thorne <ldthorne@brandeis.edu>
2016-07-17 22:34:40 -0400

    commented

--- browser/js/common/factories/UserFactory.js
+++ browser/js/common/factories/UserFactory.js
@@ -3,6 +3,7 @@
 app.factory('UserFactory', function($http) {
 	return {
 		createUser: (account) => {
+			//makes a post request to the users route and then returns the data
 			return $http.post('/api/users', account)
 			.then(res => res.data);
 		}
--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -7,6 +7,8 @@ const User = mongoose.model('User');
 module.exports = router;
 
 router.post('/', (req, res, next) => {
+	//creates a user and sends it back if successful
+	//throws a 500 and sends back the error if unsuccessful
 	User.create(req.body)
 	.then( user => res.json(user))
 	.catch(err => res.status(500).send(err));
