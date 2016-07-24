736bb6e40e3b843ba3dba9872f19b6818f515fe5
Leon Thorne <ldthorne@brandeis.edu>
2016-07-23 22:39:57 -0400

    console log

--- server/env/index.js
+++ server/env/index.js
@@ -3,6 +3,8 @@ var devConfigPath = path.join(__dirname, './development.js');
 var productionConfigPath = path.join(__dirname, './production.js');
 var stagingConfigPath = path.join(__dirname, './staging.js');
 
+console.log("NODE PROCESS IS: ",process.env.NODE_ENV )
+
 if (process.env.NODE_ENV === 'production') {
     module.exports = require(productionConfigPath);
 } else if (process.env.NODE_ENV === 'staging') {
