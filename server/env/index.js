653eee27c64b9af48717a17cc5f2485ba5085de2
Leon Thorne <ldthorne@brandeis.edu>
2016-07-23 22:41:40 -0400

    add cl

--- server/env/index.js
+++ server/env/index.js
@@ -3,6 +3,8 @@ var devConfigPath = path.join(__dirname, './development.js');
 var productionConfigPath = path.join(__dirname, './production.js');
 var stagingConfigPath = path.join(__dirname, './staging.js');
 
+console.log('\n\n\n\n\n\nENV IS', process.env.NODE_ENV)
+
 if (process.env.NODE_ENV === 'production') {
     module.exports = require(productionConfigPath);
 } else if (process.env.NODE_ENV === 'staging') {
