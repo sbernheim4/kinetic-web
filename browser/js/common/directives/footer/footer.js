699566778e43943b289c1f1e749c41a11eff754a
Sam Bernheim <sambernheim@gmail.com>
2016-07-27 09:46:00 -0400

    made footer into its own directive

---
+++ browser/js/common/directives/footer/footer.js
@@ -0,0 +1,41 @@
+app.directive('footer'), function ($rootScope, AuthService, AUTH_EVENTS, $state) {
+
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'js/common/directives/footer/footer.html',
+
+        link: function (scope) {
+
+            scope.user = null;
+
+            scope.isLoggedIn = function () {
+                return AuthService.isAuthenticated();
+            };
+
+            scope.logout = function () {
+                AuthService.logout().then(function () {
+                    $state.go('home');
+                });
+            };
+
+            const setUser = function () {
+                AuthService.getLoggedInUser()
+                .then(function (user) {
+                    scope.user = user;
+                });
+            };
+
+            const removeUser = function () {
+                scope.user = null;
+            };
+
+            setUser();
+
+            $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
+            $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
+            $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
+
+        }
+    }
+}
