d5a63ee29aa503458fb53992dd2f545f8083f8f9
Sam Bernheim <sambernheim@gmail.com>
2016-08-09 10:44:43 -0400

    boiler plate for launch page

---
+++ browser/js/launch/launch.state.js
@@ -0,0 +1,9 @@
+'use strict'
+
+app.config(function ($stateProvider) {
+	$stateProvider.state('launch', {
+		url: '/launch-a-chapter',
+		templateUrl: 'js/launch/launch.template.html',
+		controller: 'LuanchCtrl'
+	});
+});
