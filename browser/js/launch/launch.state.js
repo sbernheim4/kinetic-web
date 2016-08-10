9055d3252161c3843a150ef634cc51c1ecb03e72
Sam Bernheim <sambernheim@gmail.com>
2016-08-10 15:08:36 -0400

    added checkbox and statement + accompanying ccs + fixed spelling error in the controller

--- browser/js/launch/launch.state.js
+++ browser/js/launch/launch.state.js
@@ -4,6 +4,6 @@ app.config(function ($stateProvider) {
 	$stateProvider.state('launch', {
 		url: '/launch-a-chapter',
 		templateUrl: 'js/launch/launch.template.html',
-		controller: 'LuanchCtrl'
+		controller: 'LaunchCtrl'
 	});
 });
