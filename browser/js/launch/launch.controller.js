ec34ec6516f130002b6bff1fc4f927bf64a4571c
Sam Bernheim <sambernheim@gmail.com>
2016-08-10 21:12:31 -0400

    capturing form data in the controller when submit is pressed

--- browser/js/launch/launch.controller.js
+++ browser/js/launch/launch.controller.js
@@ -1,4 +1,21 @@
 'use strict';
 
-app.controller('LanuchCtrl', function($scope) {
+app.controller('LaunchCtrl', function($scope) {
+
+	$scope.name='';
+	$scope.email='';
+	$scope.school='';
+	$scope.classYear='';
+	$scope.questions='';
+	$scope.newsletter='';
+
+	$scope.submitFunc = function () {
+		$scope.name = this.user.name;
+		$scope.email = this.user.email;
+		$scope.school = this.user.school;
+		$scope.classYear = this.user.classYear;
+		$scope.questions = this.user.questions;
+		$scope.newsletter = this.user.newsletter;
+	}
+
 });
