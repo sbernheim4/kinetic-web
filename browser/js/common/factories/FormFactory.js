4c8b645572f01725611248d189b1da5294b8a933
Leon Thorne <ldthorne@brandeis.edu>
2016-08-20 14:57:56 -0400

    show and hide for correctly on submission

--- browser/js/common/factories/FormFactory.js
+++ browser/js/common/factories/FormFactory.js
@@ -3,13 +3,10 @@
 app.factory('FormFactory', function($http) {
 	return {
 		submitLaunchAChapterForm: (info) => {
-			//makes a post request to the users route and then returns the data
 			return $http.post('/api/forms/launch-a-chapter', info)
 			.then(res => res.data);
 		},
 		submitContactUsForm: (info) => {
-			console.log(info)
-			//makes a post request to the users route and then returns the data
 			return $http.post('/api/forms/contact-us', info)
 			.then(res => res.data);
 		}
--- browser/js/contact/contact.controller.js
+++ browser/js/contact/contact.controller.js
@@ -1,17 +1,19 @@
 'use strict';
 
 app.controller('ContactCtrl', function ($scope, FormFactory) {
+	$scope.wasFormSubmitted = false;
+
   $scope.submitForm = function (user) {
   	if(!user || !user.email || !user.message) {
   		return;
   	}
-  	
+
   	user.newsletter = !!user.newsletter;
 
   	FormFactory.submitContactUsForm(user)
   	.then( res => {
-  		console.log('success');
   		console.log(res)
+  		$scope.wasFormSubmitted = true;
   	})
   	.catch( err => {
   		console.error(err);
