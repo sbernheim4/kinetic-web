76adea8d19a731d6b6a3cd6ce34ead44672bbe0e
Sam Bernheim <sambernheim@gmail.com>
2016-08-19 22:32:19 -0400

    Use FormFactory to make the post request

--- browser/js/common/factories/FormFactory.js
+++ browser/js/common/factories/FormFactory.js
@@ -6,6 +6,11 @@ app.factory('FormFactory', function($http) {
 			//makes a post request to the users route and then returns the data
 			return $http.post('/api/forms/launch-a-chapter', info)
 			.then(res => res.data);
+		},
+		submitGetHandbookForm: (user) => {
+			console.log('in FormFactory');
+			return $http.post('/api/forms/get-the-handbook', user)
+			.then(res => res.data);
 		}
 	};
 });
--- browser/js/handbook/handbook.controller.js
+++ browser/js/handbook/handbook.controller.js
@@ -1,14 +1,19 @@
 'use strict'
 
-app.controller('HandbookCtrl', function ($scope, $http) {
+app.controller('HandbookCtrl', function ($scope, FormFactory) {
 
 	$scope.show = true;
 
 	$scope.submitGetHandbook = function (user) {
 		user.newsletter = !!user.newsletter;
 
-		console.log('Get btn pressed');
-		$http.post('/api/forms/get-the-handbook', user);
-		$scope.show = false;
+		FormFactory.submitGetHandbookForm(user)
+		.then ( () => {
+			$scope.show = false;
+		})
+		.catch (e => {
+			console.log(e);
+		});
+
 	}
 });
