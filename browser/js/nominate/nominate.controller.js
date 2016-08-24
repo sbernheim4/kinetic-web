d82f2d5646252ec112b642cbc8374c7e20ece1d9
Leon Daniel Thorne <ldthorne@brandeis.edu>
2016-08-24 00:47:32 -0400

    Revert "Temporarily revert "Add expert nomination state""

---
+++ browser/js/nominate/nominate.controller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+app.controller('NominateCtrl', function($scope, FormFactory) {
+  $scope.formSubmitted = false;
+
+  $scope.submitForm = function(data) {
+    
+    data.newsletter = !!data.newsletter;
+    data.knowsNominee = !!data.knowsNominee;
+    data.isNominee = !!data.isNominee;
+
+    FormFactory.submitExpertNominationForm(data)
+    .then(function() {
+      $scope.formSubmitted = true;
+    })
+    .catch(e => {
+      console.error(e);
+    });
+  };
+});
