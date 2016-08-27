97d88bc59c9376fefcab308fe8a4e7b2d2176bc9
Leon Thorne <ldthorne@brandeis.edu>
2016-08-27 15:15:14 -0400

    add our sponsors state

---
+++ browser/js/sponsors/sponsors.state.js
@@ -0,0 +1,10 @@
+'use strict';
+
+app.config(function ($stateProvider) {
+
+  $stateProvider.state('sponsors', {
+    url: '/our-sponsors',
+    templateUrl: 'js/sponsors/sponsors.template.html'
+  });
+
+});
