207c58738907a8c9887f961a6d0d8dd580208856
Leon Thorne <ldthorne@brandeis.edu>
2016-08-27 13:42:22 -0400

    fix indentation inconsistency

--- browser/js/template/template.state.js
+++ browser/js/template/template.state.js
@@ -1,9 +1,9 @@
 'use strict';
 
 app.config(function ($stateProvider) {
-	$stateProvider.state('template', {
-		url: '/kinetic-template',
-		controller: 'TemplateCtrl',
-		templateUrl: 'js/template/template.template.html'
-	});
+  $stateProvider.state('template', {
+    url: '/kinetic-template',
+    controller: 'TemplateCtrl',
+    templateUrl: 'js/template/template.template.html'
+  });
 });
