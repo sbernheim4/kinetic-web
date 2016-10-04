c3e7361df01683dc96d8d3ce144e0b320e971c2c
Sam Bernheim <sambernheim@gmail.com>
2016-10-04 18:43:38 -0400

    Build basics of page

---
+++ browser/js/kinetic-on-slack/slack.state.js
@@ -0,0 +1,11 @@
+'use strict'
+
+app.config(function ($stateProvider) {
+
+	// Register our *history* state.
+	$stateProvider.state('kinetic-on-slack', {
+		url: '/kinetic-on-slack',
+		controller: 'SlackCtrl',
+		templateUrl: 'js/kinetic-on-slack/slack.template.html'
+	});
+});
