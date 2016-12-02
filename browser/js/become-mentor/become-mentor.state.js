bf0adcb1825dfc81c9ff7788ddbfff610d99afe6
Samuel Bernheim <sambernheim@gmail.com>
2016-12-01 19:26:13 -0500

    Fix broken link

--- browser/js/become-mentor/become-mentor.state.js
+++ browser/js/become-mentor/become-mentor.state.js
@@ -2,7 +2,7 @@
 
 app.config(function ($stateProvider) {
 
-    // register our *campuses* state.
+    // register our *become-mentor* state.
     $stateProvider.state('become-mentor', {
         url: '/become-a-mentor',
         templateUrl: 'js/become-mentor/become-mentor.template.html',
