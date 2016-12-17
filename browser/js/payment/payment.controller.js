0b6a6822d5919216f5ebbd7e05b7f54e4be6ce32
Samuel Bernheim <sambernheim@gmail.com>
2016-12-17 17:03:27 -0500

    Refactor payment to use stripe checkout

--- browser/js/payment/payment.controller.js
+++ browser/js/payment/payment.controller.js
@@ -1,22 +1,43 @@
 'use strict'
 
-app.controller('PaymentCtrl', function ($scope) {
+app.controller('PaymentCtrl', function ($scope, FormFactory) {
 
 	$scope.cardProcessingError = false;
+	$scope.wasFormSubmitted = false;
 
-	$scope.submitForm = function (user) {
-		// launch checkout page from stripe
-		//
-		// store token that is returned as a variable and pass that
-		// user.stipeInfo =
-		FormFactory.submitDonation(user)
-		.then(() => {
-			$scope.wasFormSubmitted = true;
-		})
-		.catch(e => {
-			$scope.cardProcessingError = true;
-			console.error(e);
-		})
+	$scope.submitForm = function () {
+		showStripe();
 	};
+
+	function showStripe(){
+		// get the amount the user entered
+		const donationAmountInCents = parseInt(document.getElementById("donationAmount").value) * 100;
+
+		var handler = createHandler();
+
+		// open the handler on the page
+		handler.open({
+			name: 'Donation to Kinetic Global',
+			description: 'Donation to Kinetic Global',
+			amount: donationValueInCents
+		});
+	};
+
+	function createHandler() {
+		// Create the stripe handler --> info that is used for the popup and payment
+		var handler = StripeCheckout.configure({
+			key: 'pk_test_oMYDVrtdqS4wggcGq8FO0XNo',
+			image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+			locale: 'auto',
+			billingAddress: true,
+			token: function(token) {
+				// passes the tokenized version of all the information to FormFactory which then
+				// creates the stripe charge and charges the user
+				FormFactory.submitDonation(token)
+			}
+		});
+
+		return handler;
+	}
 
 });
