436b38cb8a63e3af48758db3dfb632612003a66f
Leon Thorne <ldthorne@brandeis.edu>
2016-12-26 19:29:36 -0500

    Add routing middleware for forum and slack routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -5,6 +5,8 @@ module.exports = router;
 // router.use('/members', require('./members'));
 router.use('/users', require('./users'));
 router.use('/forms', require('./forms'));
+router.use('/forum', require('./forum'));
+router.use('/slack', require('./slack'));
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
