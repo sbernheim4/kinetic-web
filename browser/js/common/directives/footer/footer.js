77ad29f04d4d4bb06460db7dea2399ec81173400
Leon Thorne <ldthorne@brandeis.edu>
2016-12-26 19:44:13 -0500

    Remove unnecessary request

--- browser/js/common/directives/footer/footer.js
+++ browser/js/common/directives/footer/footer.js
@@ -7,36 +7,6 @@ app.directive('footer', function ($rootScope, AuthService, AUTH_EVENTS, $state, 
 
         link: function (scope) {
             scope.goToSection = GoToSectionFactory.goToSection;
-            
-            scope.user = null;
-
-            scope.isLoggedIn = function () {
-                return AuthService.isAuthenticated();
-            };
-
-            scope.logout = function () {
-                AuthService.logout().then(function () {
-                    $state.go('home');
-                });
-            };
-
-            const setUser = function () {
-                AuthService.getLoggedInUser()
-                .then(function (user) {
-                    scope.user = user;
-                });
-            };
-
-            const removeUser = function () {
-                scope.user = null;
-            };
-
-            setUser();
-
-            $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
-            $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
-            $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
-
         }
 
     };
