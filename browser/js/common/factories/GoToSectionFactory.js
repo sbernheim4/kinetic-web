010d8aa5ff34a35e48c687b0043d116508524941
Samuel Bernheim <sambernheim@gmail.com>
2016-12-31 01:35:17 -0500

    Fix smooth scroll issue + scss fixes to nav-bar

--- browser/js/common/factories/GoToSectionFactory.js
+++ browser/js/common/factories/GoToSectionFactory.js
@@ -2,44 +2,56 @@
 
 app.factory('GoToSectionFactory', function($state) {
 	return { goToSection: (val, state) => {
-			Promise.resolve()
-			// .then ( () => {
-			// 	// close the nav bar on mobile devices before moving on
-			// 	const dropdownOne = document.querySelector('.what-is-kinetic');
-			// 	const dropdownTwo = document.querySelector('.get-involved');
-			// 	const dropdownThree = document.querySelector('.resources');
-			// 	const dropdownFour = document.querySelector('.our-team');
-			// 	const menu = document.querySelector('.big-container');
-			//
-			// 	dropdownOne.classList.remove('display');
-			// 	dropdownTwo.classList.remove('display');
-			// 	dropdownThree.classList.remove('display');
-			// 	dropdownFour.classList.remove('display');
-			// 	menu.classList.remove('active');
-			// 	menu.classList.remove('big-active');
-			// 	console.log('done!');
-			// })
-			.then( () => {
-				if (!state) {
-					state = 'home';
-				}
-				if (!val || val === "null") {
-					val = 'main';
-				}
-				if($state.current.name === state) {
-					return;
+		Promise.resolve()
+		// .then ( () => {
+		// 	// close the nav bar on mobile devices before moving on
+		// 	const dropdownOne = document.querySelector('.what-is-kinetic');
+		// 	const dropdownTwo = document.querySelector('.get-involved');
+		// 	const dropdownThree = document.querySelector('.resources');
+		// 	const dropdownFour = document.querySelector('.our-team');
+		// 	const menu = document.querySelector('.big-container');
+		//
+		// 	dropdownOne.classList.remove('display');
+		// 	dropdownTwo.classList.remove('display');
+		// 	dropdownThree.classList.remove('display');
+		// 	dropdownFour.classList.remove('display');
+		// 	menu.classList.remove('active');
+		// 	menu.classList.remove('big-active');
+		// 	console.log('done!');
+		// })
+		.then( () => {
+			if (!state) {
+				state = 'home';
+			}
+			if (!val || val === "null") {
+				val = 'main';
+			}
+			if($state.current.name === state) {
+				return;
+			} else {
+				return $state.go(state);
+			}
+		})
+		.then( () => {
+			if(val !== 'null') {
+				var el = document.querySelector(val);
+
+				var amt = 0;
+				if (window.innerWidth >= 861) {
+					// the - 100 is for the height of the nav-bar on desktop view
+					var amt = el.getBoundingClientRect().top + window.pageYOffset - 100;
+					console.log(amt);
 				} else {
-					return $state.go(state);
+					// the - 66 is for the height of the nav-bar on mobile view
+					var amt = el.getBoundingClientRect().top + window.pageYOffset;
+					console.log("MOBILE ----");
 				}
-			})
-			.then( () => {
-				if(val !== 'null') {
-					smoothScroll(document.querySelector(val));
-				}
-			})
-			.catch( err => {
-				console.error(err);
-			});
-		}
-	};
+				smoothScroll(amt);
+			}
+		})
+		.catch( err => {
+			console.error(err);
+		});
+	}
+};
 });
