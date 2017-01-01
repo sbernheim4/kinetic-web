fb7f3bbf3467f268192ab4fab7812913fe90ffab
Samuel Bernheim <sambernheim@gmail.com>
2017-01-01 13:36:26 -0500

    Close mobile navbar on smoothscroll

--- browser/js/common/factories/GoToSectionFactory.js
+++ browser/js/common/factories/GoToSectionFactory.js
@@ -3,22 +3,23 @@
 app.factory('GoToSectionFactory', function($state) {
 	return { goToSection: (val, state) => {
 		Promise.resolve()
-		// .then ( () => {
-		// 	// close the nav bar on mobile devices before moving on
-		// 	const dropdownOne = document.querySelector('.what-is-kinetic');
-		// 	const dropdownTwo = document.querySelector('.get-involved');
-		// 	const dropdownThree = document.querySelector('.resources');
-		// 	const dropdownFour = document.querySelector('.our-team');
-		// 	const menu = document.querySelector('.big-container');
-		//
-		// 	dropdownOne.classList.remove('display');
-		// 	dropdownTwo.classList.remove('display');
-		// 	dropdownThree.classList.remove('display');
-		// 	dropdownFour.classList.remove('display');
-		// 	menu.classList.remove('active');
-		// 	menu.classList.remove('big-active');
-		// 	console.log('done!');
-		// })
+		 .then ( () => {
+			 // close the nav bar on mobile devices before moving on
+			 const dropdownOne = document.querySelector('.what-is-kinetic');
+			 const dropdownTwo = document.querySelector('.get-involved');
+			 const dropdownThree = document.querySelector('.resources');
+			 const dropdownFour = document.querySelector('.our-team');
+			 const mobileNav = document.querySelector('.big-container');
+
+			 const menus = [dropdownOne, dropdownTwo, dropdownThree, dropdownFour];
+
+			 menus.map(menu => {
+				 menu.classList.remove('display');
+			 })
+
+			 mobileNav.classList.remove('active');
+			 mobileNav.classList.remove('big-active');
+		 })
 		.then( () => {
 			if (!state) {
 				state = 'home';
@@ -34,15 +35,18 @@ app.factory('GoToSectionFactory', function($state) {
 		})
 		.then( () => {
 			if(val !== 'null') {
-				var el = document.querySelector(val);
+				const el = document.querySelector(val);
 
 				var amt = 0;
+
 				if (window.innerWidth >= 861) {
-					// the - 100 is for the height of the nav-bar on desktop view
-					var amt = el.getBoundingClientRect().top + window.pageYOffset - 100;
+					const navbar = document.querySelector('.container');
+					const navbarHeight = navbar.getBoundingClientRect().height;
+					amt = el.getBoundingClientRect().top + window.pageYOffset - navbarHeight;
 				} else {
-					// the - 66 is for the height of the nav-bar on mobile view
-					var amt = el.getBoundingClientRect().top + window.pageYOffset;
+					const navbar = document.querySelector('.big-container');
+					const navbarHeight = navbar.getBoundingClientRect().height;
+					amt = el.getBoundingClientRect().top + window.pageYOffset - navbarHeight;
 				}
 				smoothScroll(amt);
 			}
@@ -50,6 +54,5 @@ app.factory('GoToSectionFactory', function($state) {
 		.catch( err => {
 			console.error(err);
 		});
-	}
-};
+	}}
 });
